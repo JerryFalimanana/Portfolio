4a434ec0f31cd146cfa165804fa9c1e50301fa1c
JerryFalimanana <ratsimbajaona1006@gmail.com>
2022-05-12 07:12:15 +0300

    theme and color switcher

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ------------------------------------ typing animation
+var typed = new Typed(".typing", {
+    strings:["", "Php Developer", "Symfony Developer", "Web Developer"],
+    typeSpeed:90,
+    BackSpeed:80,
+    loop:true
+})
